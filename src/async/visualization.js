738c0ece4daad2600b9b096b1f7d80f052396724
GrandArchTemplar <GrandArchTemplar@gmail.com>
2017-10-14 13:30:43 +0300

    changed delays for mr_kich

--- src/async/visualization.js
+++ src/async/visualization.js
@@ -23,32 +23,32 @@ async function visualize([first, second, ...points]) {
     stack = add(add(stack, first), second);
 
     drawRoundWithCircle(first, innerRadius, "blue");
-    await wait(100);
+    await wait(1);
     drawRoundWithCircle(second, innerRadius, "blue");
-    await wait(100);
+    await wait(1);
 
     for (const p of points) {
         let np = nextSTop(stack);
         let tp = sTop(stack);
         while (!isCounterClockwise(np, tp, p)) {
             drawRoundWithCircle(tp, innerRadius, "red");
-            await wait(100);
+            await wait(1);
             stack = remove(stack);
             np = nextSTop(stack);
             tp = sTop(stack);
         }
         drawRoundWithCircle(p, innerRadius, "blue");
-        await wait(100);
+        await wait(1);
         stack = add(stack, p);
     }
     stack = stack.reverse();
     for (const p of stack) {
         drawRoundWithCircle(p, innerRadius, "green");
-        await wait(50);
+        await wait(1);
     }
     let n = stack.length;
     for (let i = 0; i < n; ++i) {
         connectPoints(stack[i % n], stack[(i + 1) % n], innerRadius, "green");
-        await wait(100);
+        await wait(1);
     }
 }
